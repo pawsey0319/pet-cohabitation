9753659156c3d8cc48c3d5612e7781c33d6c699b
pawsey02 <72510210@cityu-dg.edu.cn>
2026-09-24 11:50:23 +0800

    test: scope generated-result exclusion to checkout root

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "jest-expo",
   testMatch: ["**/__tests__/**/*.test.ts?(x)"],
-  testPathIgnorePatterns: ["node_modules", "test-results"],
+  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/test-results/"],
   modulePathIgnorePatterns: ["<rootDir>/.worktrees/", "<rootDir>/test-results/"],
 };
